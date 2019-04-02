a301189dcec687666dd635d8b783218fbc911b3a
karvozavr <karvozavr@gmail.com>
2019-04-02 19:41:51 +0300

    a

--- etc/hello.py
+++ etc/hello.py
@@ -1,11 +1,13 @@
-CONFIG = {                                                                      
-    'mode': 'wsgi',                                                           
-    'working_dir': '/home/box/web',                                           
-    'python': '/usr/bin/python3',                                              
-    'args': (                                                                   
-        '--bind=0.0.0.0:8000',                                              
-        '--workers=2',                                                         
-        '--timeout=30',                                                         
-        'hello:app',                                                           
-    ),                                                                          
-} 
+CONFIG = {
+  'mode': 'wsgi',
+  'working_dir': '/home/box/web',
+  'python': '/usr/bin/python',
+  'args': (
+    '--bind=0.0.0.0:8080',
+    '--workers=2',
+    '--timeout=15',
+    '--log-level=debug',
+    '--access-logfile \'-\''
+    'hello:app'
+  )
+}
